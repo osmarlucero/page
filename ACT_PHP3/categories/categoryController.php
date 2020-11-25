2cfa18583119c16840afc8be720c01f7b493ad4d
osmarlucero <71476412+osmarlucero@users.noreply.github.com>
2020-11-24 19:53:39 -0700

    Add files via upload

---
+++ ACT_PHP3/categories/categoryController.php
@@ -0,0 +1,9 @@
+<?php
+	include "connectionController.php";
+	class CategoryController{
+		public function get(){
+			$conn = connect();
+
+		}
+	}
+?>
---
+++ ACT_PHP3/categories/index.php
@@ -0,0 +1,168 @@
+<?php
+
+	include "../app/categoryController.php";
+	$categoryController = new categoryController();
+	$categories = $categoryController->get();
+	echo json_encode($categories);
+?>
+<!DOCTYPE HTML>
+<HTML>
+	<HEAD>
+		<TITLE>
+			SITE
+		</TITLE>
+		<style type="text/css">
+			table, tr,td,th{
+				border:1px solid black;
+				 border-collapse: collapse;
+			}
+			#updateForm{
+				display: none;
+			}
+		</style>
+	</HEAD>
+	<BODY>
+		<div>
+			<h1>
+				Categories
+			</h1>
+			<?php if (isset($_SESSION) && isset($_SESSION['error'])): ?>
+					<h3>Error: <?=$_SESSION['error'] ?></h3>
+					<?php unset($_SESSION['error'])?>
+			<?php endif ?>
+			<?php if (isset($_SESSION) && isset($_SESSION['success'])): ?>
+					<h3>COrrecto: <?=$_SESSION['success'] ?></h3>
+					<?php unset($_SESSION['success'])?>
+			<?php endif ?>
+			<table>
+				<thead>
+					<th>
+						ID
+					</th>
+					<th>
+						NAME
+					</th>
+					<th>
+						DESCRIPTION
+					</th>
+					<th>
+						Actions
+					</th>
+				</thead>
+				<tbody>
+						<?php foreach ($categories as $category): ?>
+
+								<tr>
+									
+									<td>
+										<?= $category['id'] ?>
+									</td>
+									<td>
+										<?= $category['name'] ?>
+									</td>
+									<td>
+										<?= $category['description'] ?>
+									</td>
+									<td>
+										<button onclick="edit(<?= $category['id'] ?>,'<?= $category['name'] ?>',
+									'<?= $category['description'] ?>', '<?= $category['status'] ?>')">edit</button>
+										<button onclick="deleteMovie(<?= $category['id'] ?>)">DELETE</button>
+								</td>
+
+								</tr>
+									
+					<?php endforeach ?>
+				</tbody>
+			</table>
+			<form id="storeForm" action="../app/categoryController.php" method="POST">
+            <fieldset>
+
+                <legend>
+                    Add new Category
+                </legend>
+
+                <label>
+                    Name
+                </label>
+                <input type="text"  name="name" placeholder="terror" required=""> 
+                <br>
+
+                <label>
+                    Description
+                </label>
+                <textarea placeholder="write here" name="description" rows="5" required=""></textarea>
+                <br>
+
+                <label>
+                    Status
+                </label>
+                <select name="status">
+                    <option> active </option>
+                    <option> inactive </option>
+                </select>
+                <br>
+
+                <button type="submit" >Save Category</button>
+                <input type="hidden" name="action" value="store">
+            </fieldset>
+        </form>
+
+        <form id="updateForm" action="../app/categoryController.php" method="POST">
+            <fieldset>
+
+                <legend>
+                    Edit Category
+                </legend>
+
+                <label>
+                    Name
+                </label>
+                <input type="text"  id="name" name="name" placeholder="terror" required=""> 
+                <br>
+
+                <label>
+                    Description
+                </label>
+                <textarea placeholder="write here" id="description" name="description" rows="5" required=""></textarea>
+                <br>
+
+                <label>
+                    Status
+                </label>
+                <select id ="status"name="status">
+                    <option> active </option>
+                    <option> inactive </option>
+                </select>
+                <br>
+
+                <button type="submit" >Save Category</button>
+                <input type="hidden" name="action" value="update">
+                <input type="hidden" name="id" id="id">
+            </fieldset>
+        </form>
+        <form id="destroyForm" action="../app/categoryController.php" method="POST">
+        	<input type="hidden" name="action" value="delete">
+        	<input type="hidden" name="id" id="id_destroy">
+        </form>
+		</div>
+		<script type="text/javascript">
+			function edit(id,name, description,status){
+				document.getElementById('storeForm').style.display="none";
+				document.getElementById('updateForm').style.display="block";
+				document.getElementById('name').value = name;
+				document.getElementById('description').value = description;
+				document.getElementById('status').value = status;
+				document.getElementById('id').value = id;
+
+				alert("hola "+id+name+description+status);
+			}
+			function deleteMovie(id){
+				var conf=prompt("Desea eliminar, escriba:"+id);
+				if(conf== id){
+					document.getElementById('id_destroy').value = id;
+					document.getElementById('destroyForm').submit();
+				}
+			}
+		</script>
+	</BODY>
+</HTML>
